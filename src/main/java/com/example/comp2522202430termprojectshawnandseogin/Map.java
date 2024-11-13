5493ca47a870ebbb0cf1844cb3aaa42e5c126ed9
Shawn Rim <trim2@my.bcit.ca>
2024-11-12 20:49:37 -0800

    Implement sample makeBoard() method and drawBoard() method

--- src/main/java/com/example/comp2522202430termprojectshawnandseogin/Map.java
+++ src/main/java/com/example/comp2522202430termprojectshawnandseogin/Map.java
@@ -1,26 +1,60 @@
 package com.example.comp2522202430termprojectshawnandseogin;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Map {
-    private int rows;
-    private int cols;
-    private int cellSize;
-    private List<Tile> board;
+    private final int rows;
+    private final int cols;
+    private final int cellSize;
+    private final List<Tile> board;
+    private final GraphicsContext graphicsContext;
 
-    public Map(final int rows, final int cols, final int cellSize) {
+    public Map(final int rows, final int cols, final int cellSize, final GraphicsContext gc) {
         if (rows <= 0 || cols <= 0) {
             throw new IllegalArgumentException("Rows or columns must be a positive number.");
         } else if (cellSize < 0) {
             throw new IllegalArgumentException("Cell size must be bigger than or equal to zero.");
+        } else if (gc == null) {
+            throw new IllegalArgumentException("GraphicsContext is null.");
         }
 
         this.rows = rows;
         this.cols = cols;
         this.cellSize = cellSize;
         this.board = new ArrayList<Tile>();
+        this.graphicsContext = gc;
     }
 
-    public void makeBoard() {}
+    public void makeBoard() {
+        // TODO: Implement actual board later. This is for testing purpose.
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                this.board.add(new Ground(i, j));
+            }
+        }
+    }
+
+    public void drawBoard() {
+        System.out.println(board.size());
+
+        for (Tile tile : this.board) {
+            Color color;
+
+            if (tile instanceof Ground) {
+                color = Color.GREEN;
+            } else if (tile instanceof Water) {
+                color = Color.BLUE;
+            } else {
+                color = Color.SANDYBROWN;
+            }
+
+            this.graphicsContext.setFill(color);
+            this.graphicsContext.fillRect(
+                    tile.getXCoordinate() * cellSize, tile.getYCoordinate() * cellSize, cellSize, cellSize);
+        }
+    }
 }
