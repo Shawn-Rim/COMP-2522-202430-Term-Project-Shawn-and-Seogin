2b896d0d31a58e0c24fbb5972a0c91e51864c0cd
Shawn Rim <trim2@my.bcit.ca>
2024-11-12 21:03:14 -0800

    Setup JavaFX Canvas and GraphicsContext

--- src/main/java/com/example/comp2522202430termprojectshawnandseogin/Main.java
+++ src/main/java/com/example/comp2522202430termprojectshawnandseogin/Main.java
@@ -2,18 +2,27 @@ package com.example.comp2522202430termprojectshawnandseogin;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public final class Main extends Application {
-    public static final int rows = 10;
-    public static final int cols = 10;
-    public static final int cellSize = 50;
+    public static final int ROWS = 10;
+    public static final int COLS = 10;
+    public static final int CELLSIZE = 50;
+    public static Map board;
+    public static Player player;
+    public static GraphicsContext gc;
 
     @Override
     public void start(final Stage stage) throws Exception {
         VBox layout = new VBox();
-        Scene scene = new Scene(layout, rows * cellSize, cols * cellSize);
+        Canvas canvas = new Canvas(ROWS * CELLSIZE, COLS * CELLSIZE);
+        gc = canvas.getGraphicsContext2D();
+        layout.getChildren().add(canvas);
+
+        Scene scene = new Scene(layout, ROWS * CELLSIZE, COLS * CELLSIZE);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
