41e3f2e8145c69ffcef33d7accc61c2f04d51ad1
Shawn Rim <trim2@my.bcit.ca>
2024-11-21 15:12:50 -0800

    Remove code from CarrotSeed class

--- src/main/java/com/example/comp2522202430termprojectshawnandseogin/CarrotSeed.java
+++ src/main/java/com/example/comp2522202430termprojectshawnandseogin/CarrotSeed.java
@@ -2,10 +2,5 @@ package com.example.comp2522202430termprojectshawnandseogin;
 
 import java.math.BigInteger;
 
-public final class CarrotSeed extends Seed {
-    private static final BigInteger BUY_PRICE = new BigInteger("500");
-
-    public CarrotSeed(int quantity) {
-        super("Carrot Seed", BUY_PRICE, 0.2, quantity);
-    }
+public final class CarrotSeed {
 }
